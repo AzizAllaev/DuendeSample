ab1b54bd15983d0c0b7175c513ab020589b02dbe
azizallaev78@gmail.com <azizallaev78@gmail.com>
2026-02-06 23:40:05 +0500

    Little changes

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,8 +8,37 @@ namespace Client
 		{
 			var client = new HttpClient();
 
-
 			var disco = await client.GetDiscoveryDocumentAsync();
+
+			if (disco.IsError)
+			{
+				Console.WriteLine("Disco error");
+				return;
+			}
+			var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+			{
+				Address = disco.TokenEndpoint,
+				ClientId = "client",
+				ClientSecret = "secret",
+				Scope = "api1"
+			});
+			if(tokenResponse.IsError)
+			{
+				Console.WriteLine(tokenResponse.Error);
+				Console.WriteLine(tokenResponse.ErrorDescription);
+				return;
+			}
+			Console.WriteLine(tokenResponse.AccessToken);
+
+			var apiClient = new HttpClient();
+			apiClient.SetBearerToken(tokenResponse.AccessToken!);
+
+			var response = await apiClient.GetAsync("https://localhost:5001");
+
+			if (!response.IsSuccessStatusCode)
+			{
+
+			}
 		}
 	}
 }
